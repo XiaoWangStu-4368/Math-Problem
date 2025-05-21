47e24bff50225bd84ef3677e0c914caaaa9b0098
XiaoWangStu-4368 <3790169434@qq.com>
2025-05-21 20:46:46 +0800

    readjust

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,33 +5,57 @@ int main()
 {
    int s = 1;
    int answer = -1;
+   int ans_a1, ans_a2, ans_a3, ans_a4;
+   int ans_b1, ans_b2, ans_b3, ans_b4;
+   int ans_num;
    int tempa, tempb, temp;
    int b1, b2, b3, b4;
    
    for (int a1 = 7; a1 >= 0; a1--) {
 	   for (int a2 = 7;a2 >= 0; a2--) {
 		   for (int a3 = 7; a3 >= 0; a3--) {
-			   for (int a4 = 7;a4 >= 0;a4--) {
-				   cout << "Num." << s << ": ";
+			   for (int a4 = 7;a4 >= 0; a4--) {
+				   cout << "Num." << s << ":   ";
 				   b1 = 7 - a1;
 				   b2 = 7 - a2;
 				   b3 = 7 - a3;
 				   b4 = 7 - a4;
 				   tempa = 80 * a1 + 90 * a2 + 70 * a3 + 60 * a4;
 				   tempb = 100 * b1 + 120 * b2 + 110 * b3 + 70 * b4;
-				   cout << "A=" << tempa << "   " << "B=" << tempb << "   ";
-				   if (tempa > tempb) {temp = tempb;}
-				   else if (tempa == tempb) {temp = tempa;}
-				   else {temp = tempa;}
-				   cout << "finally:" << temp << "   ";
-				   if (answer <= temp) { answer = temp; }
-				   cout <<"answer=" << answer << endl;
+				   cout << "A=";
+				   cout << std::left << setw(5) << tempa;
+				   cout << "   " << "B=";
+				   cout << std::left << setw(5) << tempb;
+				   cout << "   ";
+				   if (tempa > tempb) {
+					   temp = tempb;
+				   }
+				   else {
+					   temp = tempa;
+				   }
+				   cout << "finally:" << temp << endl;
+				   if (answer <= temp) {
+                       answer = temp;
+					   ans_a1 = a1;
+					   ans_a2 = a2;
+					   ans_a3 = a3;
+					   ans_a4 = a4;
+                       ans_b1 = b1;
+					   ans_b2 = b2;
+					   ans_b3 = b3;
+					   ans_b4 = b4;
+					   ans_num = s;
+				   }
 				   s++;
 			   }
 		   }
 	   }
    }
+
+   cout << endl << endl << endl << "Answer:" << endl << "Num." << ans_num << endl;
+   cout << "      Machine1     Machine2     Machine3     MAchine4" << endl;
+   cout << "A     " << ans_a1 << "            " << ans_a2 << "            " << ans_a3 << "            " << ans_a4 << endl;
+   cout << "B     " << ans_b1 << "            " << ans_b2 << "            " << ans_b3 << "            " << ans_b4 << endl;
+   cout << "Max: " << answer << endl;
    return 0;
 }
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
